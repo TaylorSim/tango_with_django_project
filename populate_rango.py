d34b2f2b3bcf4282c109a94028052577d6443808
Taylor Sim <2658525s@student.gla.ac.uk>
2023-02-08 17:55:14 +0000

    save

--- populate_rango.py
+++ populate_rango.py
@@ -10,8 +10,11 @@ def populate():
     python_pages = [
         {'title': 'Official Python Tutorial',
          'url':'http://docs.python.org/3/tutorial/'},
-        {'title': 'How to Think Like a Computer Scientist',
-         'url': 'http://www.greenteapress.com/thinkpython/'} ]
+        {'title': 'How to Think like a Computer Scientist',
+         'url': 'http://www.greenteapress.com/thinkpython/'},
+        {'title': 'Learn Python in 10 Minutes',
+         'url': 'http://www.korokithakis.net/tutorials/python/'}
+    ]
 
     django_pages = [
         {'title': 'Official Django Tutorial',
@@ -32,7 +35,7 @@ def populate():
             'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes':16} }
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
 
