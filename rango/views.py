0068437eaf6f91cfc4cf4a9148d951a880320592
Taylor Sim <2658525s@student.gla.ac.uk>
2023-02-09 20:59:05 +0000

    chapter 7 commit

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from rango.models import Category
 from rango.models import Page
-
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
+from rango.forms import PageForm
 from django.http import HttpResponse
+from django.urls import reverse
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -28,3 +31,44 @@ def show_category(request, category_name_slug):#
         context_dict['category'] = None
         context_dict['pages'] = None
     return render(request, 'rango/category.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()#
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+        else:
+            print(form.errors)
+    return render(request, 'rango/add_category.html', {'form': form})
+
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+
+    if category is None:
+        return redirect('/rango/')
+
+    form = PageForm()
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+
+                return redirect(reverse('rango:show_category',
+                                        kwargs={'category_name_slug':
+                                                    category_name_slug}))
+            else:
+                print(form.errors)
+
+    context_dict = {'form': form, 'category': category}
+    return render(request, 'rango/add_page.html', context=context_dict)
